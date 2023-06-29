1b057e2cbebd32073ede874c0ea7c2e9d6a66eab
Nastia <adruts@mail.ru>
2023-06-30 02:10:43 +0300

    https://github.com/anastyadru/Dictionary_C.git

--- Dictionary_C/ObjectExtensions.cs
+++ Dictionary_C/ObjectExtensions.cs
@@ -16,9 +16,11 @@ namespace Dictionary_C
         public static byte[] GetBytes(this object data)
         {
             var formatter = new BinaryFormatter();
-            using var stream = new MemoryStream();
-            formatter.Serialize(stream, data);
-            return stream.ToArray();
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, data);
+                return stream.ToArray();
+            }
         }
 
         /// <summary>
@@ -30,8 +32,10 @@ namespace Dictionary_C
         public static T GetObject<T>(this byte[] data)
         {
             var formatter = new BinaryFormatter();
-            using var stream = new MemoryStream(data);
-            return (T)formatter.Deserialize(stream);
+            using (var stream = new MemoryStream(data))
+            {
+                return (T)formatter.Deserialize(stream);
+            }
         }
     }
 }
