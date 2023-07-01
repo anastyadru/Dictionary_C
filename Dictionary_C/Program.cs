658fe9fcd5841bee935e9e52eb8657eb48e2ae3d
Nastia <adruts@mail.ru>
2023-07-01 22:26:55 +0300

    https://github.com/anastyadru/Dictionary_C.git

--- Dictionary_C/Program.cs
+++ Dictionary_C/Program.cs
@@ -157,7 +157,7 @@ namespace Dictionary_C
             var storage = new Storage();
             var weatherCache = new ObservableDictionary<string, WeatherData>();
 
-            weatherCache.ItemAdded += (sender, e) =>
+            weatherCache.ItemAdded += (sender, args) =>
             {
                 var data = weatherCache[(string) sender];
                 var serializedData = JsonConvert.SerializeObject(data);
